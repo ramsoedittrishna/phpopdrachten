94594f47f69fdf49f55aef9a3c22ecac042568c2
Trishna Ramsoedit <trishna.ramsoedit@edu-kw1c.nl>
2020-02-07 14:10:19 +0100

    Dit is opdracht 2.1 en de inrichting van de werkomgeving.

---
+++ Hoofdstuk 2/opdracht2.1.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * User: Trishna Ramsoedit
+ * Date: 05-02-2020
+ * Time: 13:55 PM
+ * File: index.php
+ */
+?>
+
+<!DOCTYPE html>
+
+<header>
+    <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+</header>
+<aside>
+</aside>
+<main>
+    <p><?php echo "Hallo Wereld"; ?></p>
+</main>
