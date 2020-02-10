9e7e90d14a52f2018fd9e62bb037f7b877072f16
Trishna Ramsoedit <trishna.ramsoedit@edu-kw1c.nl>
2020-02-10 15:25:28 +0100

    opdracht 2.2 (oefening 50 % af)

---
+++ Hoofdstuk 2/opdracht22.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * User: Trishna Ramsoedit
+ * Date: 10-02-2020
+ * Time: 15:00 PM
+ * File: opdracht22.php
+ */
+?>
+<?php
+$text1 = "Hallo ";
+$text2 = "een makkelijke taal ";
+$text3 = "toch zo'n makkelijke taal ";
+$text4 = "wat is ";
+$text5 = "PHP ";
+$text6 = "Nooit gedacht dat ";
+$text7 = "De instalatie is best ingewikkeld ";
+$text8 = "Fijn ";
+$text9 = "? ";
+$text10 = ". ";
+$text11 = ", ";
+$text12 = "br";
+$text13 = "is ";
+$text14 = "Vind je niet ";
+?>
+
+<!DOCTYPE html>
+
+<header>
+    <h1><?php echo "Opdracht 2.2"; ?></h1>
+    <link rel="stylesheet" type="text/css" href="">
+</header>
+<aside>
+    <p>
+        <?php echo $text1, $text4, $text5, $text3, $text12, $text7, $text8;?>
+    </p>
+</aside>
+
+
