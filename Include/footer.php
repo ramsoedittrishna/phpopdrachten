009f0bca4ba9f2fbe02daf65575153fa9a85d24a
Trishna Ramsoedit <trishna.ramsoedit@edu-kw1c.nl>
2020-03-04 13:53:03 +0100

    opdracht 4.1 af (100%) door Trishna Ramsoedit

--- Include/footer.php
+++ Include/footer.php
@@ -11,6 +11,32 @@ include "hoofdstuk 3/variabelen.php";
 </html>
 <?php
 
-    $year;
+    date_default_timezone_set("Europe/Amsterdam");
+    echo date("d-m-y H:i");
+    $month = date("n");
     $name;
+
+
+    date_default_timezone_set("Europe/Amsterdam");
+    $uur = date("H");
+
+    if ($uur>= 0 && $uur <= 5)
+    {
+        echo "Goedennacht";
+    }
+    elseif ($uur >= 5 && $uur <= 12)
+    {
+        echo "Goedenochtend";
+    }
+    elseif ($uur >= 12 && $uur <= 17)
+    {
+        echo "Goedenmiddag";
+    }
+    elseif ($uur >= 17 && $uur <= 24)
+    {
+        echo "Goedenavond";
+    }
+
+
+
 ?>
