801829c0b3252fa4e8319f68fc8b960fc356c5e2
Trishna Ramsoedit <trishna.ramsoedit@edu-kw1c.nl>
2020-05-13 13:54:12 +0200

    7.1 Database connectie_Trishna_Ramsoedit

---
+++ hoofdstuk 7/opdracht7.1.php
@@ -0,0 +1,13 @@
+<?php
+// Open de database connectie en ODBC driver
+try
+{
+    $pdo = new PDO("odbc:odbc2sqlserver");
+}
+catch (PDOException $e)
+{
+    echo "<h1>Database error:</h1>";
+    echo $e->getMessage();
+    die();
+}
+echo "database connectie gelukt";
